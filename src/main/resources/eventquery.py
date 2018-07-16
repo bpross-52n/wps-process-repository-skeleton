50a2b36655adaac3e45dc0012188b01b58c168e5
Benjamin Pross <b.pross@52north.org>
2018-07-16 10:30:04 +0200

    Modify python script

--- src/main/resources/eventquery.py
+++ src/main/resources/eventquery.py
@@ -1,6 +1,7 @@
 import pandas
 import obspy
 import obspyDMT
+import sys
 
 
 #DUMMY DATA STUFF SHOULD BE CHANGED AS SOON AS STORAGE ETC IS FINALLY DECIDED
@@ -16,7 +17,7 @@ def connect(provider='GFZ'):
     connects to service
     '''
     if provider=='GFZ':
-        return read_database("example_event_db.csv")
+        return read_database("/home/bpr/example_event_db.csv")
 
 #FUNCTIONS
 def convert_360(lon):
@@ -53,7 +54,7 @@ def events2quakeml(events):
     pass
 
 #QUERY
-def query_events(db,lonmin=-180,lonmax=180,latmin=-90,latmax=90,mmin=0,mmax=9,zmin=0,zmax=999,p=0,etype=historic):
+def query_events(db,lonmin=-180,lonmax=180,latmin=-90,latmax=90,mmin=0,mmax=9,zmin=0,zmax=999,p=0,etype='historic'):
     '''
     Returns set of events
     type can be:
@@ -70,8 +71,13 @@ def query_events(db,lonmin=-180,lonmax=180,latmin=-90,latmax=90,mmin=0,mmax=9,zm
         - maximum depth: zmax (km, default 999)
         - probability: p (interpretation depends on type see above)
     '''
+
+    print(sys.argv)
+
     #filter type and probability
     selected = filter_type(db,etype,p)
+
+    #print(lonmin)
 
     #convert 360 degree longitude in case
     if lonmin > 180:
@@ -79,35 +85,45 @@ def query_events(db,lonmin=-180,lonmax=180,latmin=-90,latmax=90,mmin=0,mmax=9,zm
     if lonmax > 180:
         lonmax = convert_360(lonmax)
 
+    #print(lonmax)
+
     #spatial filter
     selected = filter_spatial(selected,lonmin,lonmax,latmin,latmax,zmin,zmax)
+
+    #print(selected)
 
     #magnitude filter
     selected = filter_magnitude(db,mmin,mmax)
 
+    selected.to_csv(path_or_buf='/tmp/selected.csv')    
+
+    #print(selected)
+
     #TODO convert to quakeml
-    selected=events2quakeml(selected)
+    #selected=events2quakeml(selected)
+
+    #print(selected)
 
     return selected
 
 #Program execution
+
 db = connect()
 
 #test query params
-lonmin=288
-lonmax=292
-latmin=-70
-latmax=-10
-mmin=6.6
-mmax=8.5,
-zmin=5,
-zmax=140,
-p=0,
-etype='historic'
+lonmin=float(sys.argv[1])
+lonmax=float(sys.argv[2])
+latmin=float(sys.argv[3])
+latmax=float(sys.argv[4])
+mmin=float(sys.argv[5])
+mmax=float(sys.argv[6])
+zmin=float(sys.argv[7])
+zmax=float(sys.argv[8])
+p=float(sys.argv[9])
+etype=sys.argv[10]
 #etype='deaggregation'
 #etype='stochastic'
 #etype='expert'
 #poe='likely',
 
-selected = query_events(db,lonmin,lonmax,latmin,latmax,mmin,mmax,zmin,zmax,poe,etype)
-
+selected = query_events(db,lonmin,lonmax,latmin,latmax,mmin,mmax,zmin,zmax,p,etype)
